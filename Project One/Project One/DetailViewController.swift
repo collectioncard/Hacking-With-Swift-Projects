3b14b4bfe3f6c1eeb6b5676c7a682f1adc408cfd
Thomas Wessel <turpleturtle12@gmail.com>
2020-08-04 00:29:54 -0700

    Upload Project 1
    Base project 1 is finished. The challenges portion will probably go in another folder.

---
+++ Project One/Project One/DetailViewController.swift
@@ -0,0 +1,48 @@
+//
+//  DetailViewController.swift
+//  Project One
+//
+//  Created by Thomas Wessel on 8/3/20.
+//  Copyright © 2020 Team Creative Name. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+
+    var selectedImage: String?;
+    
+    override func viewDidLoad() {
+        navigationItem.largeTitleDisplayMode = .never;
+        super.viewDidLoad()
+        title = selectedImage;
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad);
+
+        }
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true;
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false;
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Project One/Project One/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Project One
+//
+//  Created by Thomas Wessel on 8/3/20.
+//  Copyright © 2020 Team Creative Name. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    //globals
+    var pictures = [String](); // Create a String array
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        navigationController?.navigationBar.prefersLargeTitles = true;
+        title = "Storm Viewer";
+        let fileManager = FileManager.default;
+        let path = Bundle.main.resourcePath!;
+        let items = try! fileManager.contentsOfDirectory(atPath: path);
+        for item in items {
+            if item.hasPrefix("nssl"){
+                //This means that we have something that needs to be loaded.
+                pictures.append(item);
+            }
+        }
+
+        //print the picture locations to the console
+        print(pictures);
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count;
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath);
+        cell.textLabel?.text = pictures[indexPath.row];
+        return cell;
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //load the Detail view controller if it exists
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController{
+            //if we are here, we were able to get the Detail View Controller
+            vc.selectedImage = pictures[indexPath.row];
+
+            //now present this view
+            navigationController?.pushViewController(vc, animated: true);
+        }
+
+    }
+}
+
